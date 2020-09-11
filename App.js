c25818839ea354e9e7fbf907b56fed86942a031e
Balazs Danyadi <balazs.danyadi@gmail.com>
2020-09-11 14:01:49 +0200

    order view w/ order list

--- App.js
+++ App.js
@@ -4,14 +4,16 @@ import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
 import { AppLoading } from "expo";
 import * as Font from "expo-font";
-
+import ordersReducer from "./store/reducers/orders";
 import productsReducer from "./store/reducers/products";
 import cartReducer from "./store/reducers/cart";
 import ShopNavigator from "./navigation/ShopNavigator";
 import { composeWithDevTools } from "redux-devtools-extension";
+
 const rootReducer = combineReducers({
   products: productsReducer,
   cart: cartReducer,
+  orders: ordersReducer,
 });
 
 const store = createStore(rootReducer, composeWithDevTools());
